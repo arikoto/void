e0308d0317897b5047641315431d1a04c7f29110
Arikoto <162811497+arikoto@users.noreply.github.com>
2024-11-08 09:20:52 +0000

    end ...

--- app/website/src/index.js
+++ app/website/src/index.js
@@ -13,19 +13,18 @@ const server = serve({
 	port: env.PORT || 8888,
 	static: {
 		'/': await createStaticResponse('./public/index.html', 'text/html'),
-		'/css/meta.css': await createStaticResponse('./public/css/meta.css', 'text/css'),
 		'/css/footer.css': await createStaticResponse('./public/css/footer.css', 'text/css'),
 		'/css/header.css': await createStaticResponse('./public/css/header.css', 'text/css'),
 		'/css/html5-boilerplate.css': await createStaticResponse('./public/css/html5-boilerplate.css', 'text/css'),
 		'/css/index.css': await createStaticResponse('./public/css/index.css', 'text/css'),
 		'/css/main.css': await createStaticResponse('./public/css/main.css', 'text/css'),
 		'/css/normalize.css': await createStaticResponse('./public/css/normalize.css', 'text/css'),
-		'/images/roblox.png': await createStaticResponse('./public/images/roblox.png', 'image/png'),
+		'/images/image.webp': await createStaticResponse('./public/images/image.webp','image/webp'),
+		'/images/roblox.jpg': await createStaticResponse('./public/images/roblox.jpg', 'image/jpg'),
 		'/images/burger-icon.svg': await createStaticResponse('./public/images/burger-icon.svg', 'image/svg+xml'),
 		'/images/logo-alert4sud.svg': await createStaticResponse('./public/images/logo-alert4sud.svg', 'image/svg+xml'),
 		'/js/header.js': await createStaticResponse('./public/js/header.js', 'application/javascript'),
 		'/js/plugins.js': await createStaticResponse('./public/js/plugins.js', 'application/javascript'),
-		'/template.html': await createStaticResponse('./public/template.html', 'text/html'),
 	},
 	fetch(request) {
 		return new Response(null, {
